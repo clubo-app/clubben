a5a3cf75b2097892ab01fe305fae7714b57ab178
Jonas Hiltl <jonashiltl2003@gmail.com>
2022-09-10 19:39:00 +0200

    Give Party Description a Max Character Limit

--- party-service/repository/migrations/001_initial_schema.up.sql
+++ party-service/repository/migrations/001_initial_schema.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE parties (
     id varchar(27) PRIMARY KEY,
     user_id TEXT NOT NULL,
     title TEXT NOT NULL,
-    description TEXT,
+    description VARCHAR(150),
     is_public BOOLEAN NOT NULL DEFAULT false,
     max_participants INTEGER NOT NULL DEFAULT 0,
     location geometry(POINT, 4326) NOT NULL,
