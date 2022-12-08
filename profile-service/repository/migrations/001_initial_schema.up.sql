d286ee314e35126f79b7291cc78306b5f5ccd70f
Jonas Hiltl <jonashiltl2003@gmail.com>
2022-12-08 23:00:24 +0100

    [Profile] Adjust user id char constraint to 30

--- profile-service/repository/migrations/001_initial_schema.up.sql
+++ profile-service/repository/migrations/001_initial_schema.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE profiles (
-    id varchar(27) PRIMARY KEY,
+    id varchar(30) PRIMARY KEY, -- custom id from our lib. Prefix 2 char, ksuid 27, seperator 1
     username TEXT NOT NULL,
     firstname TEXT NOT NULL,
     lastname TEXT,
