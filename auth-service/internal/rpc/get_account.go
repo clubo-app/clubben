427bfb862ae74f7a1b6883789f72943eca0fd4b6
Jonas Hiltl <jonashiltl2003@gmail.com>
2022-12-16 22:06:59 +0100

    [Auth] Remove id format check

--- auth-service/internal/rpc/get_account.go
+++ auth-service/internal/rpc/get_account.go
@@ -5,15 +5,9 @@ import (
 
 	pbauth "github.com/clubo-app/clubben/auth-service/pb/v1"
 	"github.com/clubo-app/clubben/libs/utils"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (s *authServer) GetAccount(ctx context.Context, req *pbauth.GetAccountRequest) (*pbauth.Account, error) {
-	if req.Id == "" {
-		return nil, status.Error(codes.InvalidArgument, "Invalid User id")
-	}
-
 	a, err := s.accountService.GetById(ctx, req.Id)
 	if err != nil {
 		return nil, utils.HandleError(err)
