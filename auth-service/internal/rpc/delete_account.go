ca12389206a1768ce6a298597c50105dc3a2885d
Jonas Hiltl <jonashiltl2003@gmail.com>
2022-12-16 22:11:20 +0100

    [Auth] Remove id empty check

--- auth-service/internal/rpc/delete_account.go
+++ auth-service/internal/rpc/delete_account.go
@@ -5,16 +5,10 @@ import (
 
 	pbauth "github.com/clubo-app/clubben/auth-service/pb/v1"
 	"github.com/clubo-app/clubben/libs/utils"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (s *authServer) DeleteAccount(ctx context.Context, req *pbauth.DeleteAccountRequest) (*emptypb.Empty, error) {
-	if req.Id == "" {
-		return nil, status.Error(codes.InvalidArgument, "empty user id")
-	}
-
 	err := s.accountService.Delete(ctx, req.Id)
 	if err != nil {
 		return nil, utils.HandleError(err)
